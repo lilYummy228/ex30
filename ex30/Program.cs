22fee8b67563e68a7fa3eb5bbc92d54d914ac06c
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-20 21:48:55 +0500

    123

--- ex30/Program.cs
+++ ex30/Program.cs
@@ -48,7 +48,6 @@ namespace ex30
                 {'#','#','#','#','#','#','#','#','#','#','#','#','#','#'}
             };
 
-
             for (int i = 0; i < map.GetLength(0); i++)
             {
                 for (int j = 0; j < map.GetLength(1); j++)
